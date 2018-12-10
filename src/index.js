84440ad4b6ece79dc550664de87d6694c1c6ab69
AleksanderSzczuchniak <alex.szczuchniak@gmail.com>
2018-12-10 11:07:56 +0100

    jest works

--- src/index.js
+++ src/index.js
@@ -1,33 +1,6 @@
-const orders = [
-    {
-        name: 'Lager',
-        price: 10
-    },
-    {
-        name: 'APA',
-        price: 15
-    }
-]
-
-const orderTotal = (arrayOfOrders) => {
-     return arrayOfOrders.reduce(
-        (reduced, item, index, array) => {
-            reduced = reduced + item.price
-            return reduced
-        },
-        0
+export const orderTotal = (arrayOfOrders) => (
+    arrayOfOrders.reduce(
+      (reduced, item) => reduced + item.price,
+      0
     )
-}
-
-console.log(orderTotal(orders))
-
-if (
-    orderTotal(orders) !== 25
-){
-    throw new Error ('Simple sum failed!')
-}
-if (
-    orderTotal([]) !== 0
-){
-    throw new Error ('Simple sum failed coz table is empty!')
-}
+  )
